b66483770d8d5f7e6174ef7e60631890f5fa4091
Ailer20 <aronvieira820@gmail.com>
2025-08-15 15:30:19 -0400

    commit  correções de campo fornecedor

--- project/settings.py
+++ project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_select2', 
     'patrimonio',
 ]
 
@@ -75,17 +76,22 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'patrimonio',
+#        'USER': 'postgres',
+#        'PASSWORD': 'Ntech@_Ardl#n0Rt3',
+#        'HOST': '172.31.0.19',
+#        'PORT': '5432'
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'patrimonio',
-        'USER': 'postgres',
-        'PASSWORD': 'Ntech@_Ardl#n0Rt3',
-        'HOST': '172.31.0.19',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
